8248e1d8c77dcd9eaa128cb8443cfae661b1952e
homsolimpio <henrique.mengao16@gmail.com>
2020-03-02 19:52:14 -0300

    refatorando codigo

--- ftt/src/main/java/br/com/unievangelica/ftt/SisQuadrasApplication.java
+++ ftt/src/main/java/br/com/unievangelica/ftt/SisQuadrasApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FttApplication {
+public class SisQuadrasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FttApplication.class, args);
+		SpringApplication.run(SisQuadrasApplication.class, args);
 	}
 
 }
--- ftt/src/test/java/br/com/unievangelica/ftt/sisQuadrasApplicationTests.java
+++ ftt/src/test/java/br/com/unievangelica/ftt/sisQuadrasApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FttApplicationTests {
+public class sisQuadrasApplicationTests {
 
 	@Test
 	public void contextLoads() {
