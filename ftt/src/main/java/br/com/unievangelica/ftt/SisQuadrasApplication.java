8bf9925f73b245caec8ebb57d8d21911c2c88c46
homsolimpio <henrique.mengao16@gmail.com>
2020-04-02 11:38:08 -0300

    backup back end

--- ftt/src/main/java/br/com/unievangelica/ftt/SisQuadrasApplication.java
+++ ftt/src/main/java/br/com/unievangelica/ftt/SisQuadrasApplication.java
@@ -1,13 +1,23 @@
 package br.com.unievangelica.ftt;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class SisQuadrasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SisQuadrasApplication.class, args);
+	}
+
+	@RequestMapping("/home")
+	public String hello() {
+		return "Hello buddy!";
 	}
 
 }
---
+++ ftt/src/main/java/br/com/unievangelica/ftt/core/security/AccountCredentials.java
@@ -0,0 +1,13 @@
+package br.com.unievangelica.ftt.core.security;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AccountCredentials {
+
+    private String username;
+    private String password;
+
+}
