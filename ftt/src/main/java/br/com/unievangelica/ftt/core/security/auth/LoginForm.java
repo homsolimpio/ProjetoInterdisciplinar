69614451ff5cc703a218e96dd61a3267e1bd9ff2
homsolimpio <henrique.mengao16@gmail.com>
2020-04-06 19:11:38 -0300

    Gerando token

--- ftt/src/main/java/br/com/unievangelica/ftt/core/security/auth/LoginForm.java
+++ ftt/src/main/java/br/com/unievangelica/ftt/core/security/auth/LoginForm.java
@@ -2,6 +2,7 @@ package br.com.unievangelica.ftt.core.security.auth;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 @Getter
 @Setter
@@ -10,4 +11,9 @@ public class LoginForm {
     private String email;
     private String senha;
 
+
+    public UsernamePasswordAuthenticationToken converter() {
+
+        return new UsernamePasswordAuthenticationToken(email, senha);
+    }
 }
---
+++ ftt/src/main/java/br/com/unievangelica/ftt/core/security/auth/TokenService.java
@@ -0,0 +1,39 @@
+package br.com.unievangelica.ftt.core.security.auth;
+
+import br.com.unievangelica.ftt.domain.usuario.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    @Value("${sis.jwt.expiration}")
+    private String expiration;
+
+    @Value("${sis.jwt.secret}")
+    private String secret;
+
+
+    public String gerarToken(Authentication authentication) {
+
+        Usuario logado = (Usuario) authentication.getPrincipal();
+
+        Date hoje = new Date();
+        Date dataExpiracao = new Date(hoje.getTime() + Long.parseLong(expiration));
+
+        String Idlogado = Long.toString(logado.getId());
+
+        return Jwts.builder()
+                .setIssuer("Api front end SisQuadras")
+                .setSubject(Idlogado)
+                .setIssuedAt(hoje)
+                .setExpiration(dataExpiracao)
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+}
